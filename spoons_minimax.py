6f5cf6df098eb095df5222b4d8d9fa5a89f5ccbe
MatthewHodge <102556778+MatthewHodge@users.noreply.github.com>
2022-04-01 16:37:00 -0400

    outline complete
    ok so this is the first major outline
    Ran into a recursion error

--- spoons_minimax.py
+++ spoons_minimax.py
@@ -7,37 +7,8 @@
 # 5: conjoin(p1_left, p1_right)
 # 6: split(p1_left, p1_right)
 #
-from xml.etree.ElementPath import find
-
-
-def hit_spoons(self, opp):
-    opp = opp + self
-    if(opp >= 5):
-        opp = 0
-
-def split_spoons(lh, rh):
-    lh = (lh + rh) / 2
-    rh = lh
-
-def conjoin_spoons(lh, rh):
-    lh = lh + rh
-    rh = 0
-
-
-
-def turn_computer(self_left, self_right, opp_left, opp_right, move):
-    if (move == 1):
-        hit_spoons(self_left, opp_left)
-    if (move == 2):
-        hit_spoons(self_left, opp_right)
-    if (move == 3):
-        hit_spoons(self_right, opp_left)
-    if (move == 4):
-        hit_spoons(self_right, opp_right)
-    if (move == 5):
-        conjoin_spoons(self_left, self_right)
-    if (move == 6):
-        split_spoons(self_left, self_right)
+import json
+import spoons_input
 
 def find_board_state(moves):
     [p1_left, p1_right, p2_left, p2_right] = [1, 1, 1, 1]
@@ -45,9 +16,9 @@ def find_board_state(moves):
         if (i == 0):
             continue
         if (i % 2 == 1):
-            turn_computer(p1_left, p1_right, p2_left, p2_right, moves[i])
+            spoons_input.turn_computer(p1_left, p1_right, p2_left, p2_right, moves[i])
         else:
-            turn_computer(p2_left, p2_right, p1_left, p1_right, moves[i])
+            spoons_input.turn_computer(p2_left, p2_right, p1_left, p1_right, moves[i])
     return [p1_left, p1_right, p2_left, p2_right]
 
 def check_equality(board_state, turnx, boo):
@@ -88,13 +59,15 @@ def boolTest(board_state, turn_num):
     toBeReturned = []
     
     for i in range(4):
-        if (self[i / 2] != 0 and opp[i % 2] != 0):
+        if (self[int(i / 2)] != 0 and opp[i % 2] != 0):
             toBeReturned.append(i)
 
     if (sum(self) < 5):
         toBeReturned.append(5)
-    if (sum(self) % 2 == 0 and self[1] != self[2]):
+    if (sum(self) % 2 == 0 and self[0] != self[1]):
         toBeReturned.append(6)
+
+    return toBeReturned
 
 
 
@@ -118,7 +91,9 @@ def cycleTest(board_state, moves):
 def run_spoons(moves):
     board_state = find_board_state(moves)
     if(board_state[0] == 0 and board_state[1] == 0):
-        #append json file moves + \n + -1 + \n
+        # with open("spoons_minimax_info.json", "w") as write_file:
+        #    json.dump(moves, write_file)
+        #    json.dump(-1 "\\n", write_file)
         return -1
     
     if (board_state[2] == 0 and board_state[3]):
@@ -133,7 +108,7 @@ def run_spoons(moves):
     arr = boolTest(board_state, len(moves))
     test_case = []
     for i in range(len(arr)):
-        test_case[i] = run_spoons(moves + arr[i])
+        test_case[i] = run_spoons(moves + [arr[i]])
         if (test_case[i] == 1 and len(moves) % 2 == 1): # currently A's turn and has the win
             return 1
         if (test_case[i] == -1 and len(moves) % 2 == 0): # B's turn, has the win
@@ -144,20 +119,17 @@ def run_spoons(moves):
             return min(test_case)
     return 7
 
-run_spoons([0])
+with open("spoons_minimax_info.json", "w") as write_file:
+    write_file.write(str(0))
+    write_file.write("\n")
+    write_file.write(str(0))
+
+run_spoons([0, 1, 1, 1, 2, 3, 4])
 
 
 
 
 
-
-
-
-
-# TO DO: FIGURE OUT HOW TO DO POINTERS IN PYTHON?
-# Turn 0: set to 1, 1, 1, 1
-# turn 2k - 1: A turn
-# turn 2k:     B Turn
 
 
 
